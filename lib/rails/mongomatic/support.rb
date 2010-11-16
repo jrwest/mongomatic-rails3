75c3dd2ff7d12e33a1d2677faf7e492936225db0
Jordan West <jordanrw@gmail.com>
2010-11-15 20:36:58 -0800

    Errors suport and added upsert method for scaffolds

--- lib/rails/mongomatic/support.rb
+++ lib/rails/mongomatic/support.rb
@@ -3,6 +3,7 @@ module Rails
     module Support
       def self.included(base)
         base.extend ClassMethods
+        ::Mongomatic::Errors.send(:include, Errors)
       end
       
       def merge!(hash)
@@ -11,9 +12,35 @@ module Rails
         update
       end
       
+      def upsert
+        if new?
+          insert
+        else
+          update
+        end
+      end
+      
+      def upsert!
+        if new?
+          insert!
+        else
+          update!
+        end
+      end
+      
       module ClassMethods
         def model_name
           @_model_name ||= ActiveModel::Name.new(self)
+        end
+      end
+      
+      module Errors
+        def any?
+          !empty?
+        end
+        
+        def count
+          @errors.values.inject(0) { |sum, errors| sum += errors.size }
         end
       end
     end
