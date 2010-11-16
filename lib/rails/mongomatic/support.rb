72a893c440571104efba8cc6de09cd1a0e316b44
Jordan West <jordanrw@gmail.com>
2010-11-15 20:24:55 -0800

    removed unnecessary support methods added into core in 0.6.x

--- lib/rails/mongomatic/support.rb
+++ lib/rails/mongomatic/support.rb
@@ -5,22 +5,6 @@ module Rails
         base.extend ClassMethods
       end
       
-      def new_record?
-        new?
-      end
-      
-      def persisted?
-        !(new? || removed?)
-      end
-      
-      def to_key
-        (new_record?) ? nil : [to_param]
-      end
-      
-      def to_param
-        self['_id'].to_s
-      end
-      
       def merge!(hash)
         return true unless hash
         merge(hash)
